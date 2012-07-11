8ddf50dc6b8cc913d5b23c4954bd5bf0fe1b983c
Aberdeen Lab <aberdeenlab@gmail.com>
2012-07-11 14:40:46 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamlab::Application.config.secret_token = 'a005f648b3f82b2f9976d052e3cc542f002c7c8987d2167fdfce8e06a58836bc388606c0e6ee873b36ce1bf54d3771de91c7af112151bc6dabae26cd054a40be'
