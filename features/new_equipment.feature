596fff924f6164764e728ffddea4c31f0565e56a
bruce <bruce@ubuntu.(none)>
2012-07-12 16:47:24 +0100

    cucumbered

---
+++ features/new_equipment.feature
@@ -0,0 +1,7 @@
+Feature: user creates an equipment
+    Scenario: User adds an equipment
+            Given I go to the new equipment page
+            And I fillx in "name" with "toaster"
+            When I press equip "Create Equipment"
+            Then I should be on the equipment show page
+            And I should see equip "toaster"
---
+++ features/step_definitions/new_equipment_steps.rb
@@ -0,0 +1,22 @@
+Given /^I go to the new equipment page$/ do
+  visit('/equipment/new') 
+   page.should have_content("New equipment")
+end
+
+And /^I fillx in "(.*?)" with "(.*?)"$/ do |value, text|
+  value = 'equipment_'+value.downcase
+  fill_in(value,:with=>text)
+end
+
+When /^I press equip "(.*?)"$/ do |create|
+  click_button('Create Equipment')
+end 
+
+Then /^I should be on the equipment show page$/ do
+  visit('/equipment/1')
+  page.should have_content("toaster")
+end
+
+Then /^I should see equip "(.*?)"$/ do |text|
+  page.should have_content(text)
+end
