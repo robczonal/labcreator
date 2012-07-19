e8b537c076d715a712cf6d945aeab7f994b37e81
Aberdeen Lab <aberdeenlab@gmail.com>
2012-07-19 16:32:28 +0100

    devise admin added

--- db/migrate/20120718155250_devise_create_admins.rb
+++ db/migrate/20120718155250_devise_create_admins.rb
@@ -6,12 +6,6 @@ class DeviseCreateAdmins < ActiveRecord::Migration
       t.string :encrypted_password, :null => false, :default => ""
 
       t.timestamps
-    end
-
-    add_index :admins, :email,                :unique => true
-    add_index :admins, :reset_password_token, :unique => true
-    # add_index :admins, :confirmation_token,   :unique => true
-    # add_index :admins, :unlock_token,         :unique => true
-    # add_index :admins, :authentication_token, :unique => true
-  end
+	end
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120712134603) do
+ActiveRecord::Schema.define(:version => 20120718155250) do
+
+  create_table "admins", :force => true do |t|
+    t.string   "email",              :default => "", :null => false
+    t.string   "encrypted_password", :default => "", :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
+  end
 
   create_table "analyses", :force => true do |t|
     t.string   "name"
@@ -76,5 +83,28 @@ ActiveRecord::Schema.define(:version => 20120712134603) do
   end
 
   add_index "testxes", ["analysis_id"], :name => "index_testxes_on_analysis_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                                          :null => false
+    t.datetime "updated_at",                                          :null => false
+  end
+
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
