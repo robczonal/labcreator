b37073411c69c77c80ed75231251caf7d1c117cd
Kirsten <bruce@ubuntu.(none)>
2012-07-19 16:52:44 +0100

    m2m working but using checkboxes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -88,7 +88,7 @@ class ProjectsController < ApplicationController
   # PUT /projects/1
   # PUT /projects/1.json
   def update
-    params[:project][:analysis_ids] ||= []
+    params[:project][:analysis_ids]
     @project = Project.find(params[:id])
 
     respond_to do |format|
