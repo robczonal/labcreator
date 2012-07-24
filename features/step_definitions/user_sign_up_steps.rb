d527dd3b54e6535557a903c30f4a286e0a380589
Aberdeen Lab <aberdeenlab@gmail.com>
2012-07-24 14:30:36 +0100

    only admin can add user, edited breadbar

---
+++ features/step_definitions/user_sign_up_steps.rb
@@ -0,0 +1,19 @@
+Given /^I go to the home page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I pressx "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should be on the users sign_up page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I clickx "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should be on the home page$/ do
+  pending # express the regexp above with the code you wish you had
+end
