60ed02b6c4a47ac6a0eacd8b9f781e8e2b0bac45
Kirsten <bruce@ubuntu.(none)>
2012-07-24 14:28:04 +0100

    analysis-test not working yet

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -1,6 +1,6 @@
 class Analysis < ActiveRecord::Base
   attr_accessible :name, :project_ids
-  has_many :procedurexes
+  has_many :testxes
   has_and_belongs_to_many :projects
   validates :name, :presence => true
 end
