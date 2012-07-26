a2d92de2087279445e098a4db8fc87c480cf5ac0
Aberdeen Lab <aberdeenlab@gmail.com>
2012-07-26 14:44:08 +0100

    changed app name to nulab on admin backend

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -16,7 +16,7 @@ RailsAdmin.config do |config|
   # config.audit_with :paper_trail, Admin
 
   # Set the admin name here (optional second array element will appear in a beautiful RailsAdmin red ©)
-  config.main_app_name = ['Teamlab', 'Admin']
+  config.main_app_name = ['NuLab', 'Admin']
   # or for a dynamic name:
   # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
 
