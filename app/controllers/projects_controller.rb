86d6b0b2e9f3172777530bb58a419768a2247095
Aberdeen Lab <aberdeenlab@gmail.com>
2012-08-02 17:46:09 +0100

    equip tab

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -55,11 +55,12 @@ class ProjectsController < ApplicationController
     
   def equip
     @project = Project.find(params[:id])
-    @procedurex = Procedurex.find(params[:id])
 
+    
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @project }
+  
     end
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120802002836) do
+ActiveRecord::Schema.define(:version => 20120801095227) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(:version => 20120802002836) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
-
 
   create_table "admins", :force => true do |t|
     t.string   "email",              :default => "", :null => false
@@ -105,10 +104,6 @@ ActiveRecord::Schema.define(:version => 20120802002836) do
     t.string   "picture"
     t.text     "description"
     t.text     "alert"
-<<<<<<< HEAD
-=======
-    t.string   "eqtype"
->>>>>>> 6726dae848e6b202808d991caf8c6cbe42584c0f
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.integer  "equipcat_id"
