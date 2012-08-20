a23dbe678c04a4638497913fcb52cbff2486f569
Aberdeen Lab <aberdeenlab@gmail.com>
2012-08-20 15:29:52 +0100

    commit routes

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,7 @@ NuLab::Application.routes.draw do
 	resources :analyses_projects
 	resources :projects_testxes
 	resources :equipcats
+	resources :baskets
 	
   # The priority is based upon order of creation:
   # first created -> highest priority.
