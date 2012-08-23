ca1220d26567b4d4ce696be2b32a3c5839d06fc7
Kirsten <kirsten@kirsten.com>
2012-08-23 02:12:54 +0100

    Css cltered, js added but non functional

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
+    if Admin.find(:all).empty?
+      @adminemail="the administrator"
+      flash.now[:alert] = 'No administrator registered! Please contact IT support.'
+    else
+      admin=Admin.first
+      @adminemail=admin.email
 
+    end
   end
 end
