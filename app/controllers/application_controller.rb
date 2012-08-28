572ec188b08647d981e69d2a4af031b0fb59eb58
Kirsten <kirsten@kirsten.com>
2012-08-28 01:00:12 +0100

    routes changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery
   
-  def download
-    send_file "app/assets/downloads/userManual.pdf", :type=> 'application/pdf'
-  end
-  
   protected
 	def authenticate_user_or_admin!
 		unless user_signed_in? or admin_signed_in?
