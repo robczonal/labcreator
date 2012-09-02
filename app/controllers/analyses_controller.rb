1d6ed971846e17454c296d34d28d1c2b1c1f203b
Aberdeen Lab <aberdeenlab@gmail.com>
2012-09-02 23:57:40 +0100

    update

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -2,8 +2,10 @@ class AnalysesController < ApplicationController
    #This controller can be used instead of the rails admin gem to allow the administrator to update the database
    #It is used for when the administrator chooses to 'show in application' from the rails_admin gem.
 
-   #before_filter :authenticate_admin!, :only => [:new, :edit, :destroy]
-  #before_filter :authenticate_user_or_admin! 
+
+  #Admins can change, destory  and add, users can view but not change, unauthorised persons may not view
+  before_filter :authenticate_admin!, :only => [:new, :edit, :destroy]
+  before_filter :authenticate_user_or_admin! 
 
 
   
